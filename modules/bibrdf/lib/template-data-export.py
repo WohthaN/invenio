16fce2105b5061d78648048589f7ab32b01fbf4c
Samuele Carli <samuele.carli@cern.ch>
2014-04-04 14:43:40 +0200

    BibRDF: reshuffling and fixes

--- modules/bibrdf/lib/template-data-export.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import FileSystemLoader, Environment
-from datadicts.record import *
-
-
-env = Environment(loader=FileSystemLoader('./config/orcid/'), auto_reload=True)
-orcid = env.get_template('orcid.xml')
-
-
-print orcid.render({'records':[Record(1234),Record(1235)]})
