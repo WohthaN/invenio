21c95ef20fb750a121b39bf59f964ff55e5887cf
Samuele Carli <samuele.carli@cern.ch>
2014-06-24 15:54:38 +0200

    BIBRDF: tests with xml

--- modules/bibrdf/lib/bibrdf.py
+++ modules/bibrdf/lib/bibrdf.py
@@ -3,6 +3,9 @@
 from invenio.bibauthorid_dbinterface import get_all_personids_with_orcid, get_records_of_authors
 from triple_generator import generate_triples, CFG
 from template_data_export import export
+from itertools import chain
+
+from rdflib_utils import export_to_xml, transform_to_rdflib
 
 if __name__ == "__main__":
      print 'Persons'
@@ -10,17 +13,18 @@ if __name__ == "__main__":
      print 'Records'
      records = set(('record', x) for x in set(get_records_of_authors((y[1] for y in persons))))
 
-     #print 'Exporting'
-     #jobs = list(chain(persons, records))
-     #triples = generate_triples(jobs)
-     #print "Exported %s persons and %s records" % (len(persons), len(records))
+     print 'Exporting'
+     jobs = list(chain(persons, records))
+     triples = generate_triples(jobs)
+     print "Exported %s persons and %s records" % (len(persons), len(records))
+     export_to_xml(transform_to_rdflib(triples))
 
 
-     print "Generating orcid xml"
-     triples = generate_triples([list(records)[0]])
-     print triples
-     xml = export([ CFG['entity_map'][x[0]](x[1]) for x in records ], 'orcid.xml')
-     print xml
+#     print "Generating orcid xml"
+#     triples = generate_triples([list(records)[0]])
+#     print triples
+#     xml = export([ CFG['entity_map'][x[0]](x[1]) for x in records ], 'orcid.xml')
+#     print xml
 
 
 
